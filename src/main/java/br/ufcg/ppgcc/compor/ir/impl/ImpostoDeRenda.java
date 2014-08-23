22b2d5f563045dafa89c2caf36b4075c4eaf4c0b
LTI <LTI@MAQUINA-13>
2014-08-23 12:00:40 -0300

    Tarefa T.01.06 finalizada

--- src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -3,15 +3,33 @@ package br.ufcg.ppgcc.compor.ir.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.omg.CORBA.ExceptionList;
+
+import br.ufcg.ppgcc.compor.ir.ExcecaoImpostoDeRenda;
 import br.ufcg.ppgcc.compor.ir.FachadaExperimento;
 import br.ufcg.ppgcc.compor.ir.Titular;
 
 public class ImpostoDeRenda implements FachadaExperimento {
 
+	private String situaCpf;
 	private List<Titular> titulares = new ArrayList<Titular>();
 	public void criarNovoTitular(Titular titular) {
-
-		titulares.add(titular);
+		if(titular.getNome() == null){		
+			throw new ExcecaoImpostoDeRenda("O campo nome é obrigatório");	
+			
+		}
+		
+		if(titular.getCpf() == null){		
+			throw new ExcecaoImpostoDeRenda("O campo CPF é obrigatório");
+			
+		}
+		
+		if(titular.getCpf().length() != 14){		
+			throw new ExcecaoImpostoDeRenda("O campo CPF está inválido");			
+		}
+		
+			titulares.add(titular);
+					
 
 	}
 
