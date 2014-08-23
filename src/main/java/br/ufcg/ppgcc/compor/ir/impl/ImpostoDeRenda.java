36d4d25849f89ae5b7bbba3e1497bde90112cab9
LTI <LTI@MAQUINA-13>
2014-08-23 15:07:14 -0300

    Tarefa T.02.01 Finalizada

--- src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -1,18 +1,24 @@
 package br.ufcg.ppgcc.compor.ir.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.omg.CORBA.ExceptionList;
 
 import br.ufcg.ppgcc.compor.ir.ExcecaoImpostoDeRenda;
 import br.ufcg.ppgcc.compor.ir.FachadaExperimento;
+import br.ufcg.ppgcc.compor.ir.FontePagadora;
 import br.ufcg.ppgcc.compor.ir.Titular;
 
 public class ImpostoDeRenda implements FachadaExperimento {
 
 	private String situaCpf;
+	private Map<Titular, List<FontePagadora>> mapaFontes = new HashMap<Titular, List<FontePagadora>>();
 	private List<Titular> titulares = new ArrayList<Titular>();
+	private List<FontePagadora> fontes = new ArrayList<FontePagadora>();
+	
 	public void criarNovoTitular(Titular titular) {
 		if(titular.getNome() == null){		
 			throw new ExcecaoImpostoDeRenda("O campo nome é obrigatório");	
@@ -36,5 +42,20 @@ public class ImpostoDeRenda implements FachadaExperimento {
 	public List<Titular> listarTitulares() {
 		return titulares;
 	}
+
+
+	public List<FontePagadora> listarFontes(Titular titular) {
+		// TODO Auto-generated method stub
+		return mapaFontes.get(titular);
+	}
+
+
+	public void criarFontePagadora(Titular titular, FontePagadora fonte) {
+		fontes.add(fonte);
+	mapaFontes.put(titular, fontes);
+		
+	}
+
+	
 
 }
